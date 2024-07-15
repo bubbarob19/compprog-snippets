607acc8470fa9ebf08c4bb8324ef6088b46c716b
bubbarob19 <77255872+bubbarob19@users.noreply.github.com>
2024-07-14 23:28:52 -0500

    Update segtree.cpp

--- datastructures/segtree.cpp
+++ datastructures/segtree.cpp
@@ -1,44 +1,42 @@
-struct segtree {
-	int n;
-	int base;
-	vector<int> tree;
-	function<int(int,int)> merge;
+template<typename T> struct SegTree {
+	int n = 1;
+	T base;
+	vector<T> data;
+	vector<T> tree;
+	function<T(T,T)> merge;
 
-	segtree(vector<int>& arr, const function<int(int,int)>& merge, const int base) {
-		this->merge = merge;
-		this->base = base;
-		n = 1;
-		while (n < arr.size())
+	SegTree(vector<T>& data, T base, function<T(T,T)> merge) : data(data), base(base), merge(merge) {
+		while (n < data.size())
 			n <<= 1;
-		tree = vector<int>(n * 2, base);
-		for (int i = 0; i < arr.size(); i++) {
-			set(i, arr[i]);
-		}
+		tree = vector<T>(n << 1, base);
+		buildTree();
 	}
 
-	void set(int i, int val) {
-		i += n; tree[i] = val;
-		for (i >>= 1; i >= 1; i >>= 1) {
-			tree[i] = merge(tree[2*i], tree[2*i+1]);
-		}
-	}
-
-	void add(int i, int val) {
-		set(i, tree[i+n] + val);
-	}
-
-	void subtract(int i, int val) {
-		set(i, tree[i+n] - val);
-	}
-
-	int query(int l, int r) {
+	T query(int l, int r) {
 		l += n; r += n;
-		int ans = base;
+		T ans = base;
 		while (l <= r) {
 			if (l % 2 == 1) ans = merge(ans, tree[l++]);
 			if (r % 2 == 0) ans = merge(ans, tree[r--]);
 			l >>= 1; r >>= 1;
 		}
 		return ans;
+	}
+
+	T get(int i) {
+		return tree[i + n];
+	}
+
+	void update(int i, int val) {
+		i += n; tree[i] = val;
+		for (i >>= 1; i >= 1; i >>= 1)
+			tree[i] = merge(tree[2*i], tree[2*i+1]);
+	}
+
+	void buildTree() {
+		for (int i = n; i < n + data.size(); i++)
+			tree[i] = data[i-n];
+		for (int i = n-1; i >= 1; i--)
+			tree[i] = merge(tree[2*i], tree[2*i+1]);
 	}
 };
